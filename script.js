8cf9ca7574068f4dd777d1f82b91924e63200cef
Dorothy (Bauckman) Ayers <dorriesiobhan@yahoo.com>
2021-03-14 12:12:05 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+//selecting html elements
+
+let emailElement = document.querySelector('#email');
+let messageElement = document.querySelector('#message');
+let submitButton = document.querySelector('#submit-button');
+submitButton.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	let emailValue = emailElement.value;
+	let messageValue = messageElement.value;
+
+	if(emailValue.includes('@')){
+		alert('Thank you for your message!');
+	} else {
+		alert('Oh no! That does not look like a valid email address. Please try again.');
+	}
+})
+
+{let messageValue = messageElement.value;
+	if(messageValue.includes('crap')){
+		alert('NSFW!');
+	} else {
+		alert('Thank you for your message!');
+	}
+
+}
+
+console.log(validateNSFW('crap'));
